7ce5308a5e4eecf3b811a4786de842e784058b8c
RuhamaMalik <malikruhama7@gmail.com>
2025-06-18 11:33:46 +0500

    Js start , comments , connecting js files ,alert ,variables (dclare,initialize,reassign)

---
+++ js/class-01/index.js
@@ -0,0 +1,53 @@
+// window.alert('Hello from external js file');
+
+
+// var x;  // ---------- declaration
+// console.log(x); // undefined
+
+
+// var x; // declare
+// x = 'mai variable x ki value hu' // assignment / assign / initialization
+// console.log(x);
+// console.log(x);
+// console.log(x);
+
+// x = "reassign value" // reassign
+
+// console.log(x);
+// console.log(x);
+// console.log(x);
+
+// x = 'reassign second time'
+
+// console.log(x);
+// console.log(x);
+
+// console.log(">>>>>>>>>>> aik doosra statemenr");
+
+
+// console.log(x);
+
+
+
+
+///////////////////////// one step variable
+
+// var x = "decalre variable with initialization in one step";
+// // console.log(x);
+// x = "hello maza araha hai kia!";
+// // console.log(x);
+
+
+// alert(x);
+
+
+// var username = "Ruhma";
+
+// username = "Ruhama";
+
+// console.log(username);
+
+var isLoggedIn = "false";
+
+
+isLoggedIn = "true"
