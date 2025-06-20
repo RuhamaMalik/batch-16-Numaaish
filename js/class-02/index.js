449d459c7d61bd2fe40bcf59d335ce4903ef393f
RuhamaMalik <malikruhama7@gmail.com>
2025-06-20 11:31:33 +0500

    var for numbers, legal / illegal var names, , data types

---
+++ js/class-02/index.js
@@ -0,0 +1,95 @@
+// var x = 'Hum sab cheety hen......... pakka'
+// var y = "variable y"
+
+// var x = "Variable x" ; y = "variable y"
+// var x;  y;z
+
+
+// var x = "Variable x" ;
+// var y = "variable y";
+
+// console.log(x,"---------",y); // print multiple statement in one console
+// console.log(y);
+
+// username = "Ruhama"; // global variable (var)
+// console.log(username);
+
+
+
+// ////////////// Variables for Numbers # 3
+
+// var age = 10;
+// console.log(age); // 10 -- number
+// age = "My age is 10"
+// console.log(age);
+
+// var item1Price = 100;
+// var item2Price = 50; 
+
+// var total = item1Price + item2Price;
+
+// console.log("shampoo --- ", item1Price);
+// console.log("Soap --- ",item2Price);
+// console.log("total --- ", total);
+
+
+
+//////////////// DATA TYPES
+
+/*
+ 1) Primitive Data types(Immutable - not changable/ Direct Value)
+      - string
+      - number
+      - boolean(true/false)
+      - undefined
+      - Symbol
+      - null
+
+ 1) Non-Primitive Data types (Mutable - changable / reference multiple values )
+    - Array []
+    - Objects {}
+ */
+
+// var a = 10; // 10
+// var b = a; // 10
+
+// console.log(a); // 10
+// console.log(b); // 10
+
+
+// a = 5; // 5
+// b = a; // 5
+
+// console.log("value of a --- ", a); // 5
+// console.log("value of b --- ",b); // 5
+
+
+
+// var x= 10;
+// var y = x + 5;
+// console.log(y);
+
+
+
+// var x= 10;
+// var y =  5;
+// var total = x+y
+// console.log(total); // 15
+
+////////// Variable Name >>> Legal >> $, a-zA-Z, _  >> special(0-9)
+
+var username;
+var UserNamE;
+var $user$name$;
+var _user_name_;
+var username5; 
+var user1name; 
+
+
+// var username-;// illegal
+// var -username;// illegal
+// var 0username; // illegal
+// var user name; // illegal
+// var class // illegal
+// var my% // illegal
+
