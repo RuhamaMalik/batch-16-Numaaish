08a66e918593f009c401caaa55bb5485b2f0e29c
RuhamaMalik <malikruhama7@gmail.com>
2025-07-09 11:30:59 +0500

    logical opr , practise , class test

---
+++ js/class-09/index.js
@@ -0,0 +1,86 @@
+// console.log("true" && null && false);//null
+// console.log(("true" || null ) && false);//false
+// console.log( false && ("true" || undefined ));//false
+console.log(false || ("Hooria" || undefined));//true
+
+// Comparision opr (== === <=) == true\false
+// Logical opr || && == vlue
+
+// var age = +prompt("Enter your age ?")
+// if(age > 18){
+// console.log('Eligible for CNIC');
+// }else{
+
+//   console.log(' Not Eligible for CNIC');
+// }
+// console.log(age > 18 );
+
+// var marks = +prompt("Enter your marks ?")
+
+// if(marks >= 0 && marks <= 100){//outer IF 
+
+// if (marks >= 90 ) {//inner IF
+//   console.log("Grade A+");
+
+// } else if (marks >= 80 ) { // 80 -   89
+//   console.log("Grade A");
+
+// } else if (marks >= 70 ) {
+//   console.log("Grade B");
+
+// } else if (marks >= 60 ) {
+//   console.log("Grade C");
+
+// } else {
+//   console.log("Fail");
+// }
+// }else{
+//   console.log("Invalid Number");
+// }
+
+
+// letter chcking weather it is vowel or consonent
+
+// Fiza Khan
+// var alphabet = prompt("enter any alphabet");
+// if (alphabet === "a" || alphabet === "e" || alphabet === "i" || alphabet === "o" || alphabet === "u") {
+//   console.log("its a vowel");
+// }
+// else if (alphabet === "A" || alphabet === "E" || alphabet === "I" || alphabet === "O" || alphabet === "U") {
+//   console.log("its a vowel");
+
+// }
+// else {
+//   console.log("its a consonent");
+// }
+
+// Anoosha khan 
+// var alphabets = prompt("Enter any alphabets")
+// if (alphabets === "a" || alphabets === "A") {
+//     console.log("This is vowel");
+// }
+// else if (alphabets === "e" || alphabets === "E") {
+//     console.log("This is vowel");
+// }
+// else if (alphabets === "i" || alphabets === "I") {
+//     console.log("This is vowel");
+// }
+// else if (alphabets === "o" || alphabets === "O") {
+//     console.log("This is vowel");
+// }
+// else if (alphabets === "u" || alphabets === "U") {
+//     console.log("This is vowel");
+// }
+// else {
+//     console.log("This is not vowel");
+// }
+
+// Shum
+// var alpha = prompt("enter your alphabet");
+
+// if (alpha == "a" || alpha == "e" || alpha == "i" || alpha == "o" || alpha == "u" || alpha == "A" || alpha == "E" || alpha == "I" || alpha == "O" || alpha == "U") {
+//     console.log("vowel alphabet")
+// }
+// else {
+//     console.log("not vowel")
+// }
