79f036ca058b88dd9e99f6ab68c0c2cd96dd5eb0
RuhamaMalik <malikruhama7@gmail.com>
2025-07-11 11:38:19 +0500

    array , lenth ,index, methods(intro)

---
+++ js/class-10/index.js
@@ -0,0 +1,99 @@
+// var books = new Array("book1", "book2","book3");
+// console.log(books); // Array(3)
+
+// var books = ["book1", "book2","book3"]; // aray of string
+// console.log(books);
+
+// var user1 = ""
+// var user1 = ""
+// var user1 = ""
+// var user1 = ""
+
+// var myValues = ["Ruhama", 101 , true, null, [], {}, undefined, 10.5]
+// console.log(myValues); 
+
+//length --- number of elements  --- always starts with 1 //8
+
+// console.log("length >>> " + myValues.length); // 8
+
+
+// var fruits = ["apple", "banana", "mango", "kiwi", "Peach"];
+// console.log(fruits.length); // 5
+
+// console.log(fruits);
+
+// index --- position of an element  --- always starts with 0
+
+
+// console.log(fruits.length - 1); // 5 - 1  // last index
+
+
+
+// var fruits = ["apple", "banana", "", "mango", "kiwi", "Peach"];
+// console.log(fruits.length); // 6
+
+// var fruits = ["apple", "banana", , "mango", "kiwi", "Peach"];
+// console.log(fruits.length); // 6
+
+// var myArray = []; // length:0   , index:0 
+// console.log(myArray.length);
+
+// var fruits = ["apple", "banana",  "mango", "kiwi", "Peach"];
+// console.log(fruits); // length:5   , index:4
+
+// fruits = [];
+// console.log(fruits); // length:0   , index:0
+
+// fruits.length = 0;
+// console.log(fruits); // length:0   , index:0  --- []
+// console.log(fruits[3]);  // undefined
+
+// var students = ["Yusra", "Ayesha", "Sabahat", "Hafsa", "Fiza","Miss Tuba","Bisma", "Aiman", "Shumaila", "Maryam"];
+
+
+// console.log(students[5]); // Miss Tuba
+// console.log(students[8]); // Shumaila
+// console.log(students[students.length - 1]); // Maryam
+// console.log(students[students.length - 2]); // Shumaila
+// console.log(students[students.length - 3]); // Aiman
+
+// console.log(students);
+
+///////// ADD Array 
+
+
+// var myArr = []
+
+// myArr[0] = "Ruhama";
+// myArr[1] = "Gull";
+// myArr[2] = "Malik";
+// myArr[3] = "Muhammad";
+// myArr[4] = "Murtuza";
+
+// myArr[7] = "ye mera naam hai";
+// console.log(myArr);
+
+
+///////// Modify Array 
+
+// var skills = ["React", "Next js", "Html", "CSS", "babaScript", "github"];
+
+// console.log(skills[4]); // babaScript
+
+// skills[4] = "javaScript";
+
+// console.log(skills);
+
+// console.log(skills[4]); // javaScript
+
+var fruits = ["men start aya hun","apple", "banana", "", "mango", "kiwi", "Peach"];
+
+//  push -- add last
+//  pop  -- remove last
+//  unshift -- add start
+//  shift --- remove start
+//  slice
+//  splice
+
+
+
