20a62b0621df823ca3bd0979a5a10a5fe9305953
Ruhama Malik <malikruhama7@gmail.com>
2025-07-25 11:25:30 +0500

    quiz practise , string methods , while/doWhile Lopp

---
+++ js/class-16/index.js
@@ -0,0 +1,102 @@
+// console.log(('b' + 'a' + + 'a' + 'a')) // baNaNa
+// console.log( + + 'a')
+
+// console.log(typeof  null);
+
+// let ans = "6" / "3";
+
+// document.writeln ( typeof ans );
+
+
+
+// var i = 1
+// for(var i = 1; i<=10; i++){
+// if(i===5){
+//   continue; /// jump over one itration
+// }
+// console.log(i);
+// }
+
+
+//////////////////////////// while loop
+
+// // intialization variable
+// while(condition){
+//  // code
+
+//  //updation
+// }
+
+
+
+// var i = 1;
+// while(i<=10){
+// console.log(i);
+// i++;
+// }
+
+
+///////////// do while
+
+// var i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 10)
+
+
+
+//////////////// String
+
+
+// var myStr = " my Awesome StriNg";
+
+// console.log(myStr);
+// myStr=myStr.toUpperCase();
+// console.log(myStr);
+
+// console.log(myStr.toLowerCase());
+
+// var userInput = prompt("Enter vowel").toLowerCase();
+
+// console.log(userInput);
+
+// if(userInput === "a" ||
+//   userInput === "e" ||
+//   userInput === "i" ||
+//   userInput === "o" ||
+//   userInput === "u" 
+//  ){
+//  console.log("Vowel");
+// }else{
+//   console.log("consonant");
+  
+// }
+
+////////////
+
+// var myStr = " my Awesome StriNg";
+// console.log(myStr.length); // 18
+
+// var myStr = " my    Awesome    StriNg";
+// console.log(myStr.length); // 18
+// console.log(myStr);
+
+
+//  var myStr = "my 'Awesome' StriNg";
+//  console.log(myStr[3]);
+// console.log(myStr);
+//  console.log(myStr[myStr.length-1]); // g
+ 
+
+//////// indexOf --- case sensitive
+
+// var myStr = "my apple 'Awesome' StriNg . Awesome abcd, Awesome efg";
+
+// console.log(myStr.indexOf("m")); // 0
+// console.log(myStr.indexOf("s")); // 0
+// console.log(myStr.indexOf("Awesome")); // 10
+// console.log(myStr.indexOf("awesome")); // -1 --- no match found
+// console.log(myStr.lastIndexOf("Awesome")); // 42
+
+
