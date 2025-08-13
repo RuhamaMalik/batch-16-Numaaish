b1d1e9cf4252778af9e520b615c42a47f5e328cb
RuhamaMalik <malikruhama7@gmail.com>
2025-08-13 10:55:00 +0500

    revision + switch statement

---
+++ js/class-22/inex.js
@@ -0,0 +1,113 @@
+
+// if(day === "Sat" && ndknf){ /// use in compund and complex conditions
+//   console.log("Saturday");
+// }else if (day === "Sun"){
+//   console.log("Sunday");
+// }
+
+
+// var day = "Mon";
+
+// switch (day) {
+//   case "Sat":
+//   console.log("Saturday");
+//   break;
+//   case "Sun":
+//   console.log("Sunday");
+//   break;
+//   case "Mon":
+//     console.log("Monday");
+//     break;
+//   case "Tue":
+//     console.log("Tuesday");
+//     break;
+//     default:
+//       console.log("Not Found");
+// }
+
+
+
+
+// var day = prompt("Enter day!").toLowerCase() ;
+// console.log(day);
+
+// switch (day) {
+//   case "sat" :
+//   case "sun":
+//     console.log("Weekend");
+//     break;
+//   case "mon":
+//   case "tue":
+//   case "wed":
+//   case "thu":
+//   case "friday":
+//     console.log("Working day");
+//     break;
+//   default:
+//     console.log("INvalid input!");
+// }
+
+/////////////////////////////// 
+
+// var button = prompt("Press key").toUpperCase();
+
+
+// switch(button){
+// case "A":
+//   console.log("Lays");
+//   break;
+// case "B":
+//   console.log("Cola Next");
+//   break;
+// case "C":
+//   console.log("Chae");
+//   break;
+// case "D":
+//   console.log("Donuts");
+//   break;
+// case "E":
+//   console.log("water");
+//   break;
+//   default:
+//     console.log("bhikari");
+// }
+
+
+
+// var key = prompt("Press key").toUpperCase();
+
+// function vendingMachine(button) {
+//   switch (button) {
+//     case "A":
+//       return "Lays";
+//     case "B":
+//       return "Cola Next";
+//     case "C":
+//       return "Chae";
+//     case "D":
+//       return "Donuts";
+//     case "E":
+//       return "water";
+//     default:
+//       return"bhikari";
+//   }
+// }
+
+
+// var product = vendingMachine(key);
+// console.log(product);
+
+
+
+// console.log("If the".indexOf("t"));//
+// console.log("the offending phrase with the correct pharse".indexOf("t"));// 0
+// console.log("the offending phrase with the correct pharse".lastIndexOf("t"));// 36
+
+
+// single line
+
+/*
+
+multi 
+line
+*/
