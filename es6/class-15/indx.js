dcae2bb8b03e513190e87d2d62391c783a509f60
RuhamaMalik <malikruhama7@gmail.com>
2025-10-17 10:52:34 +0500

    quiz + review

---
+++ es6/class-15/indx.js
@@ -0,0 +1,40 @@
+// // ---------------local seeion storage --------------
+
+// let obj ={
+//   name:"Ruhama",// parsed
+//   age:10
+// }
+
+
+// Object.entries(obj) // [[name,ruhama],[age,10]]
+
+// `{
+//   name:"Ruhama",  stringify
+//   age:10
+// }`
+
+
+/////////////////
+
+// const obj = {}
+
+
+// console.log(obj.toString());   //
+/*  
+> etries
+> keys
+> assign
+> create
+
+> this + window
+> JSON.stringify
+> JSON.parse
+
+> shallow copy
+
+
+
+
+*/
+
+
