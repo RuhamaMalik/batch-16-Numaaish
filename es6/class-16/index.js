aef713b6ba2423d89686a84b1b84ce4d3c6994e8
RuhamaMalik <malikruhama7@gmail.com>
2025-10-20 11:08:13 +0500

    local / sesion storage

---
+++ es6/class-16/index.js
@@ -0,0 +1,63 @@
+// const myTheme = "dark";
+// const visit = 98475;
+// const isSignIn = true;
+// const user={
+//   name:"Ruhama",
+//   age:10,
+//   class:"web n app",
+//   shift:"Morning"
+// }
+
+
+
+// // console.log(JSON.stringify(user));
+
+
+// localStorage.setItem("theme", myTheme);
+// localStorage.setItem("visit", visit);
+// localStorage.setItem("signIn", isSignIn);
+// localStorage.setItem("user", JSON.stringify(user));
+
+
+
+// console.log(localStorage.getItem("theme"));
+// console.log(localStorage.getItem("visit"));
+// console.log(localStorage.getItem("signIn"));
+// // console.log(localStorage.getItem("user"));
+
+// const stringifyUser = localStorage.getItem("user");
+
+// console.log(JSON.parse(stringifyUser).name);
+
+
+// localStorage.removeItem("visit");
+
+
+
+// localStorage.clear();
+
+
+
+////////////////////////////// change theme
+
+// const theme = "light";
+
+// document.getElementById("btn")?.addEventListener("click", () => {
+//   const theme = localStorage.getItem("theme")=== "light" ? "dark" : "light";
+
+//   localStorage.setItem("theme", theme);
+// })
+
+
+// document.getElementById("clear").addEventListener("click",()=>{
+//   localStorage.removeItem("theme")
+// })
+
+
+
+// sessionStorage.setItem("count", 9847837434);
+
+//////// BRANCHES
+//////// PR(pull request) and PR Review
+//////// MERGE
+
