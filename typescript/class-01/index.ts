a809284c5f1ca6cffc6f7114cc190f6cc0254bab
RuhamaMalik <malikruhama7@gmail.com>
2025-10-29 11:06:48 +0500

    typescrip starts

---
+++ typescript/class-01/index.ts
@@ -0,0 +1,18 @@
+////////// type inference (implicit)
+
+// let a = 7;
+
+// a = 10;
+// a = "Ten" // error
+
+////////// type annotation (explicit)
+
+// let username:string = "Ruhama";
+
+// username = "Gull";
+
+// username = 55;
+
+
+
+let username;
