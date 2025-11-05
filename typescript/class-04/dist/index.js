bee8accffd996a0af7f245a9c7827755d8f9659a
RuhamaMalik <malikruhama7@gmail.com>
2025-11-05 10:01:57 +0500

    touples + oops theory + js quiz 4

---
+++ typescript/class-04/dist/index.js
@@ -0,0 +1,9 @@
+// let touple:[number,string] = [45,"abcd"];
+// // console.log(touple);
+export {};
+// // touple.push(true);
+// // touple[2]=false
+//  console.log(touple);
+/////////// oops
+/////////// class
+//# sourceMappingURL=index.js.map
---
+++ typescript/class-04/src/index.ts
@@ -0,0 +1,10 @@
+// let touple:[number,string] = [45,"abcd"];
+// // console.log(touple);
+
+// // touple.push(true);
+// // touple[2]=false
+
+//  console.log(touple);
+
+/////////// oops
+/////////// class
