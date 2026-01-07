1486e71c8edca615949f22b4b6ef579850a96830
RuhamaMalik <malikruhama7@gmail.com>
2026-01-07 11:00:48 +0500

    useEffectHook , fetchData

---
+++ React/class-01-07/src/components/hooks/Main.jsx
@@ -0,0 +1,20 @@
+import State from './state/State'
+import Products from './useEffect/Products'
+import UseEffectComponent from './useEffect/UseEffectComponent'
+
+const Main = () => {
+
+
+
+  return (
+    <>
+    
+      {/* <State /> */}
+     {/* <UseEffectComponent /> */}
+     <Products/>
+
+    </>
+  )
+}
+
+export default Main
