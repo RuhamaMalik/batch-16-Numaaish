af808465de8b1acb34a0ea7f68db00d2255f6d0f
RuhamaMalik <malikruhama7@gmail.com>
2026-02-06 11:09:21 +0500

    vite + tailwind

---
+++ React/class-18/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     
+     <h1 className='text-indigo-700 text-[20rem] bg-indigo-200 text-shadow-lg  text-shadow-black' >Hello tailwind</h1>
+     
+    </>
+  )
+}
+
+export default App
