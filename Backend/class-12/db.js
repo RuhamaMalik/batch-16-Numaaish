a1010a7be7027f3d9827ebbffc88f86b1dcfc879
RuhamaMalik <malikruhama7@gmail.com>
2026-04-20 11:11:39 +0500

    db crud

--- Backend/class-12/db.js
+++ Backend/class-12/db.js
@@ -1,13 +1,13 @@
 const { MongoClient } = require('mongodb');
 
 
-const uri = "mongodb+srv://hafizaaqeela:hafizaaqeela2026@cluster0.zlat2hl.mongodb.net/?appName=Cluster0";
+const uri = "mongodb+srv://batch16:batch162026@cluster0.vnpmvkr.mongodb.net/?appName=Cluster0";
 const client = new MongoClient(uri);
 
 const connectDb = async () => {
   await client.connect();
-  console.log("Connected successfully to server");
-  const database = client.db('myDatabase');
+  console.log("DB Connected successfully to server");
+  const database = client.db('e-com');
 
   return database;
 }
--- Backend/class-12/index.js
+++ Backend/class-12/index.js
@@ -29,26 +29,93 @@
 
 
 
+// const db = require('./db');
+
+
+
+// const checkDb = async () => {
+//   try {
+
+//     const userRef = (await db()).collection('users');
+
+//     const result = await userRef.insertOne({
+//       name: 'Ayesha',
+//       email: 'ayesha@gmail.com',
+//       role: 'user'
+//     })
+
+//     console.log(result);
+
+//   } catch (err) {
+//     console.log(err);
+
+//   } 
+// }
+// checkDb()
+
+
+/////////////////// create
+
+
+const express = require('express');
 const db = require('./db');
+const { ObjectId } = require('mongodb');
+const app = express();
 
+app.use(express.json());
 
+app.get('', (req, res) => {
+  res.send('Home')
+});
 
-const checkDb = async () => {
+//////////// create product
+
+app.post('/api/products', async (req, res) => {
   try {
+    const productRef = (await db()).collection('products');
+    const result = await productRef.insertOne(req.body);
+    // const result = await productRef.insertMany(req.body);
 
-    const userRef = (await db()).collection('users');
+    if (result.acknowledged) {
+      res.status(201).send({ message: "Product created!" })
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "ERROR in Product" })
+  }
+});
 
-    const result = await userRef.insertOne({
-      name: 'Ayesha',
-      email: 'ayesha@gmail.com',
-      role: 'user'
-    })
+//////////// get all products
 
-    console.log(result);
+app.get('/api/products', async (req, res) => {
+  const productRef = (await db()).collection('products');
+  const data = await productRef.find().toArray();
+  res.status(200).json({ message: "Products get successfully!", data })
+})
 
-  } catch (err) {
-    console.log(err);
+//////////  update product
 
-  } 
-}
-checkDb()
+app.patch('/api/products/:title', async (req, res) => {
+  const productRef = (await db()).collection('products');
+  const result = await productRef.updateOne(
+    { title: req.params.title },
+    {
+      $set: req.body
+    });
+
+  res.json({ message: "product updated", result });
+})
+
+
+//////////  delete product
+
+app.delete('/api/products/:id', async (req, res) => {
+  const productRef = (await db()).collection('products');
+ const result= productRef.deleteOne(
+    { _id: new ObjectId(req.params.id) }
+  );
+
+  res.json({message:'Prduct deleted',result})
+})
+
+app.listen(8000, () => console.log('Server is Running'));
